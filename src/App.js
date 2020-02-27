8497fa657f2d09f8f1542d32fa723e747bbebe39
better-peter <email@nupponen.com>
2020-02-26 19:26:07 -0800

    Routes started to add... continues

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 //import logo from './logo.svg';
 //import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+        {/* <Route path='/topics/:topicId' component={TopicDetail}></Route> */}
+      </Switch>
     </div>
   );
 }
