6e47dc7b5d73291488a37da3535422d10202d50d
better-peter <email@nupponen.com>
2020-03-05 20:49:05 -0800

    added cart add functionality

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,10 @@
 import { CartActionTypes } from './cart.types';
+//import { addItemToCart, removeItemFromCart } from './cart.utils';
+import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
-  hidden: true
+  hidden: true,
+  cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -11,6 +14,16 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         hidden: !state.hidden
       };
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload)
+      };
+    // case CartActionTypes.REMOVE_ITEM:
+    //   return {
+    //     ...state,
+    //     cartItems: removeItemFromCart(state.cartItems, action.payload)
+    //   };
 
     default:
       return state;
