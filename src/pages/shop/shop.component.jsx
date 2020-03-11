b27462c842ab22235f446ff875e287ee6d0acee8
better-peter <email@nupponen.com>
2020-03-10 20:47:57 -0700

    viewing collection using category string to find category id and then back

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import CollectionOverview from '../../components/collections-overview/collections-overview.component';
 
-const ShopPage = () => (
-  <div className='shop-page'>
-    <CollectionOverview />
-  </div>
-);
+import CollectionPage from '../collection/collection.component';
+
+const ShopPage = ({ match }) => {
+  console.log(match);
+  return (
+    <div className='shop-page'>
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+  );
+};
 
 export default ShopPage;
