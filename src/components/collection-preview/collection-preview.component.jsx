08ea36857b2668707f3506421e709682f8d36cc5
better-peter <email@nupponen.com>
2020-03-11 16:43:56 -0700

    updated tiny error with dollar sign and added links to preview page

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({ title, items }) => (
-  <div className='collection-preview'>
-    <h1 className='title'>{title.toUpperCase()}</h1>
-    <div className='preview'>
-      {items
-        .filter((item, idx) => idx < 4)
-        .map(item => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
+const CollectionPreview = ({ title, items }) => {
+  const newLink = '/shop/' + title.toLowerCase();
+  return (
+    <div className='collection-preview'>
+      <Link to={newLink}>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+      </Link>
+
+      <div className='preview'>
+        {items
+          .filter((item, idx) => idx < 4)
+          .map(item => (
+            <CollectionItem key={item.id} item={item} />
+          ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default CollectionPreview;
+
+{
+  /* <Link className='title' to=`/shop/${{title.toUpperCase()} </Link> */
+}
