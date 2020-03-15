e5a9d95d59e9f1af177811b9cc30442c8548b382
better-peter <email@nupponen.com>
2020-03-15 11:26:46 -0700

    Added spinner and data receiving from BE.

---
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { SpinnerContainer, SpinnerOverlay } from './with-spinner.styles';
+
+const WithSpinner = WrappedComponent => {
+  const Spinner = ({ isLoading, ...props }) => {
+    return isLoading ? (
+      <SpinnerOverlay>
+        <SpinnerContainer />
+      </SpinnerOverlay>
+    ) : (
+      <WrappedComponent {...props} />
+    );
+  };
+  return Spinner;
+};
+
+// const WithSpinner = WrappedComponent => ({ isLoading, ...props }) => {
+//     return isLoading ? (
+//       <SpinnerOverlay>
+//         <SpinnerContainer />
+//       </SpinnerOverlay>
+//     ) : (
+//       <WrappedComponent {...props} />
+//     );
+//   };
+
+export default WithSpinner;
